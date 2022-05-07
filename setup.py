19175e2ad10e103bd29c9b9b3a4f07e570c1ad1d
somal <somalkant.mnnit@gmail.com>
2022-05-07 14:37:49 +0530

    webstructue added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src_new",
+    version="0.0.1",
+    description="its a new wine Q package",
+    author="somalkant",
+    packages=find_packages(),
+    license="MIT"
+)
