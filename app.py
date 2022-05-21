2a1341b40731b094aa2d044bb3605f03998d8f94
Somal Kant <somalkant.mnnit@gmail.com>
2022-05-21 16:14:00 +0530

    updated prediction.py

--- app.py
+++ app.py
@@ -1,33 +1,15 @@
-from flask import Flask,render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify
 import os
-import yaml
-import joblib
 import numpy as np
+from prediction_service import prediction
 
-params_path = "params.yaml"
+
 webapp_root = "webapp"
 
 static_dir = os.path.join(webapp_root, "static")
 template_dir = os.path.join(webapp_root, "templates")
 
 app = Flask(__name__, static_folder=static_dir,template_folder=template_dir)
-
-def read_params(config_path):
-    with open(config_path) as yaml_file:
-        config = yaml.safe_load(yaml_file)
-    return config
-
-def predict(data):
-    config = read_params(params_path)
-    model_dir_path = config["webapp_model_dir"]
-    model = joblib.load(model_dir_path)
-    prediction = model.predict(data)
-    print(prediction)
-    return prediction[0]
-
-def api_response(request):
-    pass
-
 
 
 @app.route("/", methods=["GET", "POST"])
